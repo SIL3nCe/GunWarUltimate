ec17a0e3ec00789e1fe288a2f42265f262b588bb
Kranck <frizzy.rastay@gmail.com>
2019-10-05 15:34:08 +0200

    Renamed CharacterController script to PlayerCharacterController

--- Assets/Scripts/Character/PlayerCharacterController.cs
+++ Assets/Scripts/Character/PlayerCharacterController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class PlayerCharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
