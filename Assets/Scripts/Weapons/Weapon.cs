6e3caa0b05a2b9bd9b75e89100fde389df4dff15
Johann NOVAK <johann.novak25@gmail.com>
2019-10-05 23:41:06 +0200

    handled drop force for weapon and blink

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -9,5 +9,61 @@ public enum EWeaponType
 
 public class Weapon : MonoBehaviour
 {
-	public EWeaponType WeaponType;
+	public EWeaponType	WeaponType;
+
+	//
+	// Dropped-related
+	private bool		bDropped = false;
+	public float		ThresholdBlink = 7.0f;
+	public float		ThresholdBlinking = 0.25f;
+	public float		ThresholdDisappear = 10.0f;
+	private float		fDroppedAccumulatedDuration = 0.0f;
+	private MeshRenderer[]	rendererArray;
+
+	private void Start()
+	{
+		rendererArray = GetComponentsInChildren<MeshRenderer>();
+	}
+
+	private void Update()
+	{
+		if(bDropped)
+		{
+			float fDelta = Time.deltaTime;
+			fDroppedAccumulatedDuration += fDelta;
+			if (fDroppedAccumulatedDuration < ThresholdBlink)
+			{
+				// ...
+			}
+			else if (fDroppedAccumulatedDuration < ThresholdDisappear)
+			{
+				float fBlinkingDuration = fDroppedAccumulatedDuration - ThresholdBlink;
+				int iBlinkingDurationRemainder = (int)(fBlinkingDuration / ThresholdBlinking);
+				if (iBlinkingDurationRemainder % 2 == 0)
+				{
+					foreach(MeshRenderer meshRenderer in rendererArray)
+					{
+						meshRenderer.enabled = false;
+					}
+				}
+				else
+				{
+					foreach (MeshRenderer meshRenderer in rendererArray)
+					{
+						meshRenderer.enabled = true;
+					}
+				}
+			}
+			else
+			{
+				Destroy(gameObject);
+				fDroppedAccumulatedDuration = 0.0f;
+			}
+		}
+	}
+
+	public void Drop()
+	{
+		bDropped = true;
+	}
 }
