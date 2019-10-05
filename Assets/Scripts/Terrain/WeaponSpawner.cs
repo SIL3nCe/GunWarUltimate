b53f38c158785a2e25a9a5f98863cca65862f87e
Th0re <stephane.perrez@gmail.com>
2019-10-06 01:14:03 +0200

    Added tests for a weapon spawner

---
+++ Assets/Scripts/Terrain/WeaponSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public GameObject m_spawnLocation;
+    public GameObject m_weaponPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(m_weaponPrefab, m_spawnLocation.transform.position, gameObject.transform.rotation);
+    }
+}
