d0b7dbe9f46fc4a0150f44a085007af7cfea1b39
Johann NOVAK <johann.novak25@gmail.com>
2019-10-06 15:32:11 +0200

    added spawner behaviour

---
+++ Assets/Scripts/Terrain/PlayerSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+	public GameObject[] SpawnerLocation;
+
+	public Transform GetSpawnLocation()
+	{
+		int iIndex = (int)(Random.value * (SpawnerLocation.Length-1));
+		return SpawnerLocation[iIndex].transform;
+	}
+}
