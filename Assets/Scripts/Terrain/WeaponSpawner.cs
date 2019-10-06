dbc9f6d3a44647c139555ce8af5e22c6e42df100
Johann NOVAK <johann.novak25@gmail.com>
2019-10-06 16:37:46 +0200

    added weapon spawner

--- Assets/Scripts/Terrain/WeaponSpawner.cs
+++ Assets/Scripts/Terrain/WeaponSpawner.cs
@@ -4,23 +4,48 @@ using UnityEngine;
 
 public class WeaponSpawner : MonoBehaviour
 {
-    public GameObject m_spawnLocation;
-    public GameObject m_weaponPrefab;
+    public GameObject[]	spawnLocations;
+    public GameObject[]	weaponPrefabs;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int MaxWeaponCount = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float ThresholdNewWeapon = 5.0f;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Instantiate(m_weaponPrefab, m_spawnLocation.transform.position, gameObject.transform.rotation);
-    }
+	private float fDurationNewWeapon = 0.0f;
+	private int iCurrentWeaponCount = 0;
+
+	void Update()
+	{
+		fDurationNewWeapon += Time.deltaTime;
+		if(fDurationNewWeapon >= ThresholdNewWeapon)
+		{
+			if(iCurrentWeaponCount < MaxWeaponCount)
+			{
+				SpawnRandomWeapon();
+
+				fDurationNewWeapon = 0.0f;
+			}
+		}
+	}
+
+	public void SpawnRandomWeapon()
+	{
+		GameObject spawnLocation = spawnLocations[(int)(Random.value * (spawnLocations.Length - 1))];
+		GameObject weaponPrefab = weaponPrefabs[(int)(Random.value * (weaponPrefabs.Length - 1))];
+
+		SpawnWeapon(spawnLocation, weaponPrefab);
+	}
+
+	public void SpawnWeapon(GameObject spawnLocation, GameObject weaponPrefab)
+	{
+		GameObject newWeapon = Instantiate<GameObject>(weaponPrefab, spawnLocation.transform.position, spawnLocation.transform.localRotation);
+		newWeapon.GetComponent<Rigidbody>().velocity = spawnLocation.transform.right * spawnLocation.GetComponent<WeaponSpawnLocation>().initialVelocity;
+
+		++iCurrentWeaponCount;
+	}
+
+	public void OnWeaponDisappeared()
+	{
+		--iCurrentWeaponCount;
+	}
 }
