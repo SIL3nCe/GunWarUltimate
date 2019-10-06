b05bc18f0d74dfc75d4ca1799a130da5f95a8eb1
Thomas <tom.pradal@aliceadsl.fr>
2019-10-06 13:15:27 +0200

    Added life time on bullets

--- Assets/Scripts/Weapons/BulletGameplay.cs
+++ Assets/Scripts/Weapons/BulletGameplay.cs
@@ -7,35 +7,64 @@ public class BulletGameplay : MonoBehaviour
     [Tooltip("Prefab to Instantiate on hit")]
     public GameObject HitEffect;
 
-    private float damages; // Set by weapon on shot
+    [Tooltip("Time before bullet desappear if no hit")]
+    public float LifeTime;
+
+    private float Damages; // Set by weapon on shot
+    private bool IsRocket;
+    
+    void Start()
+    {
+        IsRocket = null != HitEffect && HitEffect.name.Contains("Rocket");
+
+        Invoke("OnBulletLifeTimeSpend", LifeTime);
+    }
 
     public void SetDamages(float InDamages)
     {
-        damages = InDamages;
+        Damages = InDamages;
+    }
+
+    void OnBulletLifeTimeSpend()
+    {
+        if (IsRocket)
+        {
+            TriggerHitEffect();
+        }
+        Destroy(gameObject);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (null != HitEffect)
+        if (IsRocket)
         {
-            Vector3 bulletLocation = gameObject.transform.position;
-            Quaternion bulletRotation = gameObject.transform.rotation;
-            GameObject hitExplosion = Instantiate(HitEffect, bulletLocation, bulletRotation);
-            RocketExplosion rocketScript = hitExplosion.GetComponent<RocketExplosion>();
-            if (null != rocketScript)
-            {
-                rocketScript.SetDamages(damages);
-            }
+            TriggerHitEffect();
         }
         else
         {
             PlayerGameplay player = collision.gameObject.GetComponent<PlayerGameplay>();
             if (null != player)
             {
-                player.TakeDamages(damages);
+                player.TakeDamages(Damages);
+                TriggerHitEffect();
             }
         }
 
         Destroy(gameObject);
+    }
+
+    private void TriggerHitEffect()
+    {
+        if (null != HitEffect)
+        {
+            Vector3 bulletLocation = gameObject.transform.position;
+            Quaternion bulletRotation = gameObject.transform.rotation;
+            GameObject hitEffectObject = Instantiate(HitEffect, bulletLocation, bulletRotation);
+            RocketExplosion rocketScript = hitEffectObject.GetComponent<RocketExplosion>();
+            if (null != rocketScript)
+            {
+                rocketScript.SetDamages(Damages);
+            }
+        }
     }
 }
