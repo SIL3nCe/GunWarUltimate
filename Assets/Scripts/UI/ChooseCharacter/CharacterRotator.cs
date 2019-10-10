06dc4486aeb125aaf6cce55db557f0601b7ac76a
Johann NOVAK <johann.novak25@gmail.com>
2019-10-10 20:38:25 +0200

    added CharacterRotator to UIChooseCharacter

---
+++ Assets/Scripts/UI/ChooseCharacter/CharacterRotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotator : MonoBehaviour
+{
+    private Transform transformInit;
+    private const int iSpeed = 20;
+
+    private void Start()
+    {
+        transformInit = transform;
+    }
+
+    private void Update()
+    {
+        float fDt = Time.deltaTime;
+        transform.Rotate(iSpeed * Vector3.up * fDt);
+    }
+
+    public void ResetRotation()
+    {
+        transform.SetPositionAndRotation(transformInit.position, transform.rotation);
+    }
+}
