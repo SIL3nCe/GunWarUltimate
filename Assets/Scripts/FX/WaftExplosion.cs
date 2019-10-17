a9f36fb111a8303278aaf2400df1a036ca57e71e
Thomas <tom.pradal@aliceadsl.fr>
2019-10-18 00:14:25 +0200

    Created script for waft effect, avoid damages on waft owner

---
+++ Assets/Scripts/FX/WaftExplosion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaftExplosion : MonoBehaviour
+{
+    public float damages;
+    public float ejectionFactor;
+
+    void Start()
+    {
+        Invoke("DisableCollisions", 0.1f);
+    }
+
+    void DisableCollisions()
+    {
+        gameObject.GetComponent<SphereCollider>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerGameplay player = other.gameObject.GetComponent<PlayerGameplay>();
+        if (null != player && other.gameObject != transform.parent.gameObject)
+        {
+            Vector3 dir = player.transform.position - transform.position;
+            player.TakeDamages(damages, dir, ejectionFactor);
+        }
+    }
+}
