4728062968ee3087b1e3413c9f061ac31febd0b6
Kranck <frizzy.rastay@gmail.com>
2019-10-17 23:51:27 +0200

    Cleaned the PlayerController and added more parameters. Started wallcast

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Assertions;
 
+[System.Serializable]
+public class PlayerControllerCollisionsOptions
+{
+    [Header("Ground")]
+    [Tooltip("The offset vertically that is used from the real player position to check for the ground. This value is automatically" +
+        " inverted (* -1) to check under the player.")]
+    public float m_fGroundCheckOffset = 1.0f;
+    [Tooltip("The radius of the sphere that is used in a physic raycast to detect ground")]
+    public float m_fGroundCheckRadius = 0.2f;
+
+    [Header("Walls")]
+    [Tooltip("The offset from the player position to check for walls in the two directions. In fact this value is applied to the X position of the player" +
+        " in + and - direction to check for collisions in front of the player and backward.")]
+    public float m_fWallCheckHorizontalOffset = 0.6f;
+    [Tooltip("Identical to the check offset for the ground check, it offsets the vertical position of the checking of the walls")]
+    public float m_fWallCheckVerticalOffset = 0.0f;
+    [Tooltip("The radius of the sphere that is used to check for walls")]
+    public float m_fWallCheckRadius = 0.2f;
+
+    [Header("Layers")]
+    [Tooltip("The layers to ignore for the raycast to determine if the player hits the ground or a wall")]
+    // If all bits in the layerMask are on, we will collide against all colliders. If the layerMask = 0, we will never find any collisions with the ray.
+    // Because here we want to raycast against al colliders except this one, we have to invert the bitmask, this is done in the start method
+    public LayerMask m_ignoredLayersMask;
+}
+
 [RequireComponent(typeof(PlayerInput), typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
@@ -17,15 +43,14 @@ public class PlayerController : MonoBehaviour
     [Header("Movements")]
     [Tooltip("The player speed. Relative value, in fact it is a multiplicator with the inputs for the movements.")]
     public float m_fPlayerSpeed = 1.0f;
+    [Tooltip("The height of a jump of the player when he is grounded (in meters).")]
     public float m_fJumpHeight = 1.0f;
+    [Tooltip("The height of the second jump the player can do while in air (in meters)")]
+    public float m_fDoubleJumpHeight = 1.0f;
 
     //
-    // Ground check
-    [Header("Ground Check")]
-    [Tooltip("The layers to ignore for the raycast to determine if the player is grounded or not")]
-    // If all bits in the layerMask are on, we will collide against all colliders. If the layerMask = 0, we will never find any collisions with the ray.
-    // Because here we want to raycast against al colliders except this one, we have to invert the bitmask, this is done in the start method
-    public LayerMask m_groundedIgnoreLayersMask;
+    // Collisions Options
+    public PlayerControllerCollisionsOptions m_collisionsOptions;
 
 
     //--------------------
@@ -53,17 +78,19 @@ public class PlayerController : MonoBehaviour
         Assert.AreNotEqual(m_rigidbody, null);
 
         //
-        // We have to invert the bitmask for the collision layers for the ground raycast
-        // This is don with '~'. By doing this we will process the collisions with all the collisions mask 
-        // except the one(s) specified in the variable
-        m_groundedIgnoreLayersMask = ~m_groundedIgnoreLayersMask;
+        // We have to invert the bitmask for the collision layers 
+        // This is done with '~'. By doing this we will process the collisions with all the collisions mask 
+        // except the one(s) specified in the variable. This is useful because we want to do the raycast
+        // with everything EXCEPT the players (layermask Character) to avoid detecting collisions (like ground and walls) when the sphere
+        // checks overlap with the player capsule.
+        m_collisionsOptions.m_ignoredLayersMask = ~m_collisionsOptions.m_ignoredLayersMask;
     }
 
     void Update()
     {
         //
         // Raycast to know if we are grounded
-        if (Physics.CheckSphere(transform.position + new Vector3(0.0f, -1.0f, 0.0f), 0.2f, m_groundedIgnoreLayersMask))
+        if (Physics.CheckSphere(transform.position + new Vector3(0.0f, -m_collisionsOptions.m_fGroundCheckOffset, 0.0f), m_collisionsOptions.m_fGroundCheckRadius, m_collisionsOptions.m_ignoredLayersMask))
         {
             m_bGrounded = true;
 
@@ -76,6 +103,18 @@ public class PlayerController : MonoBehaviour
             m_bGrounded = false;
         }
 
+        //
+        // We check if we are colliding with a wall
+        // The check depends on the move input, because we only check in the direction the player is actually moving.
+        // In fact the player can only start hanging on a wall when going in the direction of that wall
+        if (Physics.CheckSphere(transform.position + new Vector3(m_collisionsOptions.m_fWallCheckHorizontalOffset * m_vMoveInput.x, m_collisionsOptions.m_fWallCheckVerticalOffset, 0.0f), m_collisionsOptions.m_fWallCheckRadius, m_collisionsOptions.m_ignoredLayersMask))
+        {
+            Debug.Log("Wall cast ! ");
+        }
+        else
+        {
+            //Debug.Log("")
+        }
     }
 
     public void OnMove(InputValue inputValue)
@@ -123,11 +162,9 @@ public class PlayerController : MonoBehaviour
                 //
                 //
                 // The only difference is that we reduce the jump height.
-                // For now we simply divide the jump height by 2, but we could add a parameter to control this as a second jump height.
-                // TODO: If we want to configure a double jump height, add the variable and insert the value here instead of the calculation based
-                // on the initial jump height
+                // The jump height is a parameter, generally it is < to the normal jump height
                 // Add force for the jump
-                m_rigidbody.AddForce(new Vector3(0.0f, Mathf.Sqrt((m_fJumpHeight / 2f) * -2f * Physics.gravity.y), 0.0f), ForceMode.VelocityChange);
+                m_rigidbody.AddForce(new Vector3(0.0f, Mathf.Sqrt(m_fDoubleJumpHeight * -2f * Physics.gravity.y), 0.0f), ForceMode.VelocityChange);
                 //
                 // Do not forget to remove the double jump flag, we double jumped, we cant double jump anymore
                 m_bCanDoubleJump = false;
@@ -145,8 +182,15 @@ public class PlayerController : MonoBehaviour
     public void OnDrawGizmos()
     {
         //
-        // Draw grounded sphere
-        Gizmos.DrawSphere(transform.position + new Vector3(0.0f, -1.0f, 0.0f), 0.2f);
+        // Draw ground check sphere
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position + new Vector3(0.0f, -m_collisionsOptions.m_fGroundCheckOffset, 0.0f), m_collisionsOptions.m_fGroundCheckRadius);
+
+        //
+        // Draw wall check sphere
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + new Vector3(m_collisionsOptions.m_fWallCheckHorizontalOffset, m_collisionsOptions.m_fWallCheckVerticalOffset, 0.0f), m_collisionsOptions.m_fWallCheckRadius);
+        Gizmos.DrawWireSphere(transform.position + new Vector3(-m_collisionsOptions.m_fWallCheckHorizontalOffset, m_collisionsOptions.m_fWallCheckVerticalOffset, 0.0f), m_collisionsOptions.m_fWallCheckRadius);
     }
 
 }
