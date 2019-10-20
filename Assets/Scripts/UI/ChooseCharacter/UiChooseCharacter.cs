41fe25b6a774aa2f1526d35b6958fcc4067e24da
Thomas <tom.pradal@aliceadsl.fr>
2019-10-20 22:46:40 +0200

    Use u/d/r/l for charac selection

--- Assets/Scripts/UI/ChooseCharacter/UiChooseCharacter.cs
+++ Assets/Scripts/UI/ChooseCharacter/UiChooseCharacter.cs
@@ -39,7 +39,14 @@ public class UiChooseCharacter : MonoBehaviour
 		public GameObject	character;
 		public string		strName;
 		public Material		material;
-	}
+
+        public Character(GameObject charac, string name, Material mat)
+        {
+            character = charac;
+            strName = name;
+            material = mat;
+        }
+    }
 	private Character[]	CharacterArray;
 
 	//
@@ -56,93 +63,105 @@ public class UiChooseCharacter : MonoBehaviour
 	}
 	private Selector[] aSelectors;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		//
-		// Create character array
-		CharacterArray = new Character[8];
-		CharacterArray[0].character = CharacterJudy;		CharacterArray[0].strName = "Judy";			CharacterArray[0].material = MaterialJudy;
-		CharacterArray[1].character = CharacterDropos;		CharacterArray[1].strName = "Dropos";		CharacterArray[1].material = MaterialDropos;
-		CharacterArray[2].character = CharacterMargharet;	CharacterArray[2].strName = "Margharet";	CharacterArray[2].material = MaterialMargharet;
-		CharacterArray[3].character = CharacterYannis;		CharacterArray[3].strName = "Yannis";		CharacterArray[3].material = MaterialYannis;
-		CharacterArray[4].character = CharacterRambi;		CharacterArray[4].strName = "Rambi";		CharacterArray[4].material = MaterialRambi;
-		CharacterArray[5].character = CharacterBilly;		CharacterArray[5].strName = "Billy";		CharacterArray[5].material = MaterialBilly;
-		CharacterArray[6].character = CharacterR3D5;		CharacterArray[6].strName = "R3D5";			CharacterArray[6].material = MaterialR3D5;
-		CharacterArray[7].character = CharacterJoe;			CharacterArray[7].strName = "Joe";			CharacterArray[7].material = MaterialJoe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        // Create character array
+        CharacterArray = new Character[8];
+        CharacterArray[0] = new Character(CharacterDropos, "Dropos", MaterialDropos);
+        CharacterArray[1] = new Character(CharacterYannis, "Yannis", MaterialYannis);
+        CharacterArray[2] = new Character(CharacterBilly, "Billy", MaterialBilly);
+        CharacterArray[3] = new Character(CharacterJoe, "Joe", MaterialJoe);
+        CharacterArray[4] = new Character(CharacterJudy, "Judy", MaterialJudy);
+        CharacterArray[5] = new Character(CharacterMargharet, "Margharet", MaterialMargharet);
+        CharacterArray[6] = new Character(CharacterRambi, "Rambi", MaterialRambi);
+        CharacterArray[7] = new Character(CharacterR3D5, "R3D5", MaterialR3D5);
 
-		//
-		// Create selector array
-		int iPlayerCount = System.Enum.GetValues(typeof(EPlayerEnum)).Length;
-		aSelectors = new Selector[iPlayerCount];
-		for(int iPlayerIndex = 0; iPlayerIndex < iPlayerCount; ++iPlayerIndex)
-		{
-			aSelectors[iPlayerIndex].iSelectionIndex = -1;
-			aSelectors[iPlayerIndex].bSelected = false;
-		}
-		aSelectors[0].color = Color.red;
-		aSelectors[1].color = Color.blue;
-		aSelectors[0].highlightRef = HighLight1;
-		aSelectors[1].highlightRef = HighLight2;
-		aSelectors[0].checkmark = CheckmarkP1;
-		aSelectors[1].checkmark = CheckmarkP2;
-		aSelectors[0].preview = CharacterPreviewP1;
-		aSelectors[1].preview = CharacterPreviewP2;
-		aSelectors[0].name = CharacterNameP1;
-		aSelectors[1].name = CharacterNameP2;
+        //
+        // Create selector array
+        int iPlayerCount = System.Enum.GetValues(typeof(EPlayerEnum)).Length;
+        aSelectors = new Selector[iPlayerCount];
+        for (int iPlayerIndex = 0; iPlayerIndex < iPlayerCount; ++iPlayerIndex)
+        {
+            aSelectors[iPlayerIndex].iSelectionIndex = -1;
+            aSelectors[iPlayerIndex].bSelected = false;
+        }
+        aSelectors[0].color = Color.red;
+        aSelectors[1].color = Color.blue;
+        aSelectors[0].highlightRef = HighLight1;
+        aSelectors[1].highlightRef = HighLight2;
+        aSelectors[0].checkmark = CheckmarkP1;
+        aSelectors[1].checkmark = CheckmarkP2;
+        aSelectors[0].preview = CharacterPreviewP1;
+        aSelectors[1].preview = CharacterPreviewP2;
+        aSelectors[0].name = CharacterNameP1;
+        aSelectors[1].name = CharacterNameP2;
     }
 
-	// Update is called once per frame
-	void Update()
-	{
-		//
-		// Selection Movement
-		if (!aSelectors[0].bSelected)
-		{
-			if (Input.GetKeyUp(KeyCode.Q))				{	MoveSelectionPrevious(ref aSelectors[0]);	}
-			if (Input.GetKeyUp(KeyCode.D))				{	MoveSelectionNext(ref aSelectors[0]);		}
-		}
-		if (!aSelectors[1].bSelected)
-		{
-			if (Input.GetKeyUp(KeyCode.LeftArrow))		{	MoveSelectionPrevious(ref aSelectors[1]);	}
-			if (Input.GetKeyUp(KeyCode.RightArrow))		{	MoveSelectionNext(ref aSelectors[1]);		}
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        // Selection Movement
+        // TODO Use InputAction here
+        if (!aSelectors[0].bSelected)
+        {
+            if (Input.GetKeyUp(KeyCode.Q))
+                MoveSelectionPrevious(ref aSelectors[0], 1);
+            if (Input.GetKeyUp(KeyCode.D))
+                MoveSelectionNext(ref aSelectors[0], 1);
+            if (Input.GetKeyUp(KeyCode.S))
+                MoveSelectionPrevious(ref aSelectors[0], 4);
+            if (Input.GetKeyUp(KeyCode.Z))
+                MoveSelectionNext(ref aSelectors[0], 4);
+        }
 
-		//
-		// Selection
-		if (Input.GetKeyUp(KeyCode.Keypad0))	{	SelectCharacter(ref aSelectors[1]);			}
-		if (Input.GetKeyUp(KeyCode.Space))		{	SelectCharacter(ref aSelectors[0]);			}
+        if (!aSelectors[1].bSelected)
+        {
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+                MoveSelectionPrevious(ref aSelectors[1], 1);
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+                MoveSelectionNext(ref aSelectors[1], 1);
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+                MoveSelectionPrevious(ref aSelectors[1], 4);
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+                MoveSelectionNext(ref aSelectors[1], 4);
+        }
 
-		//
-		// Start condition
-		bool bStart = true;
-		foreach (Selector s in aSelectors)
-		{
-			bStart &= s.bSelected;
-		}
-		if (bStart)
-		{
-			Invoke("LaunchGame", 3.0f);
-		}
-	}
+        //
+        // Selection
+        if (Input.GetKeyUp(KeyCode.Keypad0))
+            SelectCharacter(ref aSelectors[1]);
+        if (Input.GetKeyUp(KeyCode.Space))
+            SelectCharacter(ref aSelectors[0]);
 
-	private void MoveSelectionNext(ref Selector selector)
+        //
+        // Start condition
+        bool bStart = true;
+        foreach (Selector s in aSelectors)
+        {
+            bStart &= s.bSelected;
+        }
+        if (bStart)
+        {
+            Invoke("LaunchGame", 3.0f);
+        }
+    }
+
+	private void MoveSelectionNext(ref Selector selector, int valInc)
 	{
 		if (-1 == selector.iSelectionIndex)
 		{
 			selector.preview.SetActive(true);
 			selector.iSelectionIndex = 0;
-			selector.highlightRef.SetActive(true);
-			selector.highlightRef.GetComponent<Image>().color = selector.color;
-			selector.highlightRef.transform.SetPositionAndRotation(CharacterArray[selector.iSelectionIndex].character.transform.position, CharacterArray[selector.iSelectionIndex].character.transform.rotation);
+            selector.highlightRef.SetActive(true);
+            selector.highlightRef.transform.SetPositionAndRotation(CharacterArray[selector.iSelectionIndex].character.transform.position, CharacterArray[selector.iSelectionIndex].character.transform.rotation);
             selector.highlightRef.transform.position = new Vector3(selector.highlightRef.transform.position.x, selector.highlightRef.transform.position.y, 10.0f);
         }
 		else
 		{
-			selector.preview.SetActive(true);
-			selector.iSelectionIndex = (selector.iSelectionIndex + 1) % CharacterArray.Length;
-			selector.highlightRef.SetActive(true);
-			selector.highlightRef.GetComponent<Image>().color = selector.color;
+			selector.iSelectionIndex = (selector.iSelectionIndex + valInc) % CharacterArray.Length;
 			selector.highlightRef.transform.SetPositionAndRotation(CharacterArray[selector.iSelectionIndex].character.transform.position, CharacterArray[selector.iSelectionIndex].character.transform.rotation);
             selector.highlightRef.transform.position = new Vector3(selector.highlightRef.transform.position.x, selector.highlightRef.transform.position.y, 10.0f);
         }
@@ -150,23 +169,19 @@ public class UiChooseCharacter : MonoBehaviour
 		UpdateCharacterPreview(selector);
 	}
 
-	private void MoveSelectionPrevious(ref Selector selector)
+	private void MoveSelectionPrevious(ref Selector selector, int valInc)
 	{
 		if (-1 == selector.iSelectionIndex)
 		{
 			selector.preview.SetActive(true);
 			selector.iSelectionIndex = CharacterArray.Length - 1;
 			selector.highlightRef.SetActive(true);
-			selector.highlightRef.GetComponent<Image>().color = selector.color;
 			selector.highlightRef.transform.SetPositionAndRotation(CharacterArray[selector.iSelectionIndex].character.transform.position, CharacterArray[selector.iSelectionIndex].character.transform.rotation);
             selector.highlightRef.transform.position = new Vector3(selector.highlightRef.transform.position.x, selector.highlightRef.transform.position.y, 10.0f);
         }
 		else
 		{
-			selector.preview.SetActive(true);
-			selector.iSelectionIndex = ((selector.iSelectionIndex - 1) + CharacterArray.Length) % CharacterArray.Length;
-			selector.highlightRef.SetActive(true);
-			selector.highlightRef.GetComponent<Image>().color = selector.color;
+			selector.iSelectionIndex = ((selector.iSelectionIndex - valInc) + CharacterArray.Length) % CharacterArray.Length;
 			selector.highlightRef.transform.SetPositionAndRotation(CharacterArray[selector.iSelectionIndex].character.transform.position, CharacterArray[selector.iSelectionIndex].character.transform.rotation);
             selector.highlightRef.transform.position = new Vector3(selector.highlightRef.transform.position.x, selector.highlightRef.transform.position.y, 10.0f);
         }
