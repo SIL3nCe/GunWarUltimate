759287444899eaa5becb6464321371a68316b2f7
Thomas <tom.pradal@aliceadsl.fr>
2019-10-27 02:27:25 +0200

    Added cursors on champselect. Added scripts for cursor logic, runner gameplay. Is currently a clone generator

---
+++ Assets/Scripts/UI/ChooseCharacter/CharacterSelection.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using UnityEngine;
+
+// Script used with cursors on runner champ select
+
+public class CharacterSelection : MonoBehaviour
+{
+    [Header("Characters")]
+    public BoxCollider2D[] CharacterBoxes;
+
+    [Header("Cursors")]
+    public RawImage[] CursorList;
+
+    [Header("Runner ref")]
+    public RunnerGameplay RunnerScript;
+
+    //TODO array of device or deviceId, null by default and same size than CursorList
+
+    // Screen values for cursor limits
+    private float fHalfHeight;
+    private float fScreenWidth, fScreenHeight;
+
+    void Start()
+    {
+        Assert.IsTrue(CursorList.Length == 8);
+        Assert.IsTrue(CharacterBoxes.Length == 8);
+
+        fScreenWidth = Screen.width;
+        fScreenHeight = Screen.height;
+        fHalfHeight = fScreenHeight * 0.5f;
+    }
+    
+    void Update()
+    {
+        // Move cursors
+        // TODO check for move inputs, move relevant cursor based on device id
+
+        // Special case of mouse for P1
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        { 
+            Vector3 vMouseLocation = Input.mousePosition;
+            KeepInCharacterPanel(ref vMouseLocation);
+            CursorList[0].transform.position = vMouseLocation;
+        }
+
+
+        // Validate action
+        // TODO handle all devices (based on input which try valide)
+        int nCharac = CharacterBoxes.Length;
+        for (int i = 0; i < nCharac; ++i)
+        {
+            if (CharacterBoxes[i].bounds.Contains(CursorList[0].transform.position))
+            {
+                RunnerScript.OnPlayerValidated(i); // TODO replace i with player id
+            }
+        }
+
+        //TODO
+        // Cancel action
+        //RunnerScript.OnPlayerCanceled(playerId);
+    }
+
+    private void KeepInCharacterPanel(ref Vector3 location)
+    {
+        location.x = Mathf.Max(location.x, 0.0f);
+        location.x = Mathf.Min(location.x, fScreenWidth);
+        location.y = Mathf.Max(location.y, fHalfHeight);
+        location.y = Mathf.Min(location.y, fScreenHeight);
+    }
+}
---
+++ Assets/Scripts/UI/RunnerGameplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerGameplay : MonoBehaviour
+{
+    [Header("Character to spawn")]
+    public GameObject CharacPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void OnPlayerValidated(int playerId)
+    {
+        Instantiate(CharacPrefab, new Vector3(0.0f, -2.0f, 0.5f), Quaternion.identity);
+    }
+
+    public void OnPlayerCanceled(int playerId)
+    {
+
+    }
+}
